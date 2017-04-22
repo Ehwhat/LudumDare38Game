726da34c79a45f26091274b258d78242ab86ae21
Joshua Stevenson <JoshuaStevenson97@gmail.com>
2017-04-22 19:02:44 +0100

    Small Dash implimented

--- Assets/Scripts/Movement/ShipMovementController.cs
+++ Assets/Scripts/Movement/ShipMovementController.cs
@@ -5,33 +5,27 @@ using UnityEngine;
 public class ShipMovementController : SphericalMovementController {
 
 
-    public float _minMovementSpeed = 20f;
-    public float _maxMovementSpeed = 100f;
-    public float _turnSpeed = 2f;
-    public float _accelrationPerSecond = 10f;
+    public float _movementSpeed = 20f;
 
+    public float _dashModifier = 2f;
+    public float _dashTime = 1f;
+    public float _dashCooldown = 5f;
+
+    public float _turnSpeed = 2f;
     public int _requiredBoost = 3;
 
-    public float[] _speedNotches = {
-        50,
-        20,
-        -20
-    };
-
-    public int _currentSpeedNotch = 1;
-
-    private float _lastSpeedChangeTime;
     public float _currentSpeed;
 
     private float _originalRadius;
-
+    private float _timeSinceLastDash;
+    private bool _isDashing;
 
     private Vector2 _axisInput;
 
     void Start()
     {
         base.Start();
-        _lastSpeedChangeTime = Time.time;
+        _timeSinceLastDash = -_dashCooldown;
         _originalRadius = _radius;
     }
 
@@ -39,15 +33,12 @@ public class ShipMovementController : SphericalMovementController {
 	void Update () {
 
         _axisInput = new Vector2(-Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        if (Input.GetKeyDown(KeyCode.W))
+        
+        if(_axisInput.y > 0.5f)
         {
-            IncreaseSpeed();
+            StartDash();
         }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            DecreaseSpeed();
-        }
-        //Debug.Log(GetCurrentSpeed());
+
         ShipMovementStep();
         
     }
@@ -65,33 +56,38 @@ public class ShipMovementController : SphericalMovementController {
         _radius = _originalRadius;//+((WaterController.instance.DistanceToWater(new Vector3(_positionOnPlane.x/2, 0, _positionOnPlane.y/2), Time.time))*20)+10f;
     }
 
+    void StartDash()
+    {
+        if (!_isDashing)
+        {
+            if(Time.time - (_timeSinceLastDash) > _dashCooldown + _dashTime)
+            {
+                _isDashing = true;
+                _timeSinceLastDash = Time.time;
+            }
+        }
+    }
+
     float GetCurrentSpeed()
     {
-        _currentSpeed = Mathf.Lerp(_currentSpeed, CalculateShipSpeed(), Time.deltaTime * _accelrationPerSecond);
+        _currentSpeed = Mathf.Lerp(_currentSpeed, CalculateShipSpeed(), Time.deltaTime * 20);
         return _currentSpeed;
     }
 
     float CalculateShipSpeed()
     {
-        return _speedNotches[_currentSpeedNotch];
-    }
-
-    void DecreaseSpeed()
-    {
-        if(_currentSpeedNotch < _speedNotches.Length-1)
+        float result = _movementSpeed;
+        if (_isDashing)
         {
-            _currentSpeedNotch++;
-            _lastSpeedChangeTime = Time.time;
+            if (Time.time - _timeSinceLastDash < _dashTime)
+            {
+                result *= _dashModifier;
+            }else
+            {
+                _isDashing = false;
+            }
         }
-    }
-
-    void IncreaseSpeed()
-    {
-        if(_currentSpeedNotch > 0)
-        {
-            _currentSpeedNotch--;
-            _lastSpeedChangeTime = Time.time;
-        }
+        return result;
     }
 
 }

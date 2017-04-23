b9257143448cc88c4db283b62503c31eeeedeac4
AlloyOfFiction <alloyoffiction@gmail.com>
2017-04-23 19:16:19 +0200

    Simple menu + ship w' colours
    (ship_02 is the proper one)

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+    
+    public void LoadScene(int level)
+        // can I just write onclick? Hum. Purhaps I should not attempt this... yet. I remember far too little. 
+    {
+        Application.LoadLevel(level);
+    }
+		
+}
